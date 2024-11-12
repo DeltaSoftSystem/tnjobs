c9a37366ac34ab663d99da31d60da2028f214a49
Faizan Alam <alamf6023@gmail.com>
2024-11-12 13:23:13 +0530

    route updated

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,18 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use Mews\Captcha\Captcha;
+
+Route::get('/captcha-image', [Captcha::class, 'create']);
 
 Route::redirect('/','/description');
-Route::get('/description',function(){
-    return view('pages.description');
-});
-Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('registerpage');
+Route::get('/description',function(){ return view('pages.description'); })->name('description');
+Route::get('/landing_page',function(){ return view('pages.landing_page');})->name('description2');
 
 Route::get('/verify-otp', [AuthController::class, 'showOtpForm'])->name('otp.verify');
 Route::post('/verify-otp', [AuthController::class, 'verifyOtp'])->name('otp.check');
 
-
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('registerpage');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('loginpage');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
